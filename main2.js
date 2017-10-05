51f509a792035ce9bb76ac7ace7489e7dca30ee0
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-05 19:00:02 +0200

    Add max cells

--- main2.js
+++ main2.js
@@ -2,7 +2,10 @@
 let monTableau = document.querySelector('#tableau');
 
 //get the number of cells per line/row
-let tabSize = parseInt (prompt ("Quelle dimension de la grille veux-tu?"));
+let tabSize = parseInt(prompt("Quelle dimension de la grille veux-tu?"));
+
+//max 70 cells per line/row
+if (tabSize > 70) tabSize = 70;
 
 //fill the container
 for (var iter = 1; iter <= tabSize*tabSize; iter++) {
