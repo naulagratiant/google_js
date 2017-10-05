f20626146f1ab41c9474ad36c8d432007ffc11f1
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-05 17:29:45 +0200

    make the table dynamic

--- main2.js
+++ main2.js
@@ -1,8 +1,8 @@
 let monTableau = document.querySelector('#tableau');
 
-
 let tabSize = parseInt (prompt ("Quelle dimension de la grille veux-tu?"));
 for (var iter = 1; iter <= tabSize*tabSize; iter++) {
-  monTableau.innerHTML += '<div class = "divcel">'+ iter + '</div>'
+	monTableau.innerHTML += '<div id="divcel' + iter +'" class = "divcel"></div>'
+	document.querySelector('#divcel'+iter).style.width = (300/tabSize) + "px";
+	document.querySelector('#divcel'+iter).style.height = (300/tabSize) + "px";
 }
-
