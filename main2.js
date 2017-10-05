92d55028c04277af0b546c9adfdae4518f72eb69
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-05 17:48:50 +0200

    Improve performance

--- main2.js
+++ main2.js
@@ -1,8 +1,20 @@
 let monTableau = document.querySelector('#tableau');
 
 let tabSize = parseInt (prompt ("Quelle dimension de la grille veux-tu?"));
+let mesCellules = "";
 for (var iter = 1; iter <= tabSize*tabSize; iter++) {
-	monTableau.innerHTML += '<div id="divcel' + iter +'" class = "divcel"></div>'
-	document.querySelector('#divcel'+iter).style.width = (300/tabSize) + "px";
-	document.querySelector('#divcel'+iter).style.height = (300/tabSize) + "px";
+	mesCellules += '<div class = "divcel divcelsize"></div>'
 }
+monTableau.innerHTML = mesCellules;
+
+var S = document.createElement('style');
+S.setAttribute("type", "text/css");
+var cl1 = '.divcelsize { width: ' + (300/tabSize) + 'px; height: ' + (300/tabSize) + 'px; }';
+if (S.styleSheet){
+	S.styleSheet.cssText=cl1;
+}
+else{ 
+	cl1 = document.createTextNode(cl1); 
+	S.appendChild(cl1);
+}
+document.body.appendChild(S);
